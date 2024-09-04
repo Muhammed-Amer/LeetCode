4f1ef8ba75668014cb6651289b7b08c717468fec
Muhammed-Amer <muhammedamer7810@gmail.com>
2024-09-04 20:57:26 +0300

    [1929] [Time Beats: 96.25%] [Memory Beats: 32.86%] - LeetPush

---
+++ Arrays/Easy/1929-Concatenation-of-Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n*2];
+        for(int i=0; i<nums.length; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+}
