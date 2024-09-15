f5823a86f770a1953b5f233e12963ac3a4926a14
Muhammed-Amer <muhammedamer7810@gmail.com>
2024-09-15 08:01:22 +0300

    [35] [Time Beats: 100.00%] [Memory Beats: 53.05%] - LeetPush

---
+++ Searching/Easy/35-Search-Insert-Position.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        if(target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
