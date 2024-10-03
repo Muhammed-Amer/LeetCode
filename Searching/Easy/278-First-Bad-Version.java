98443d70feaf7425f0d319adf7e70182d0cacf33
Muhammed-Amer <muhammedamer7810@gmail.com>
2024-10-03 18:11:57 +0300

    [278] [Time Beats: 73.14%] [Memory Beats: 81.79%] - LeetPush

---
+++ Searching/Easy/278-First-Bad-Version.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        int res = 0;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            boolean isBad = isBadVersion(mid);
+
+            if(isBad) {
+                res = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } 
+        return res;
+    }
+}
